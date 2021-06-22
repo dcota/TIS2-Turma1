392495b1e48f20f3dd7f57f43fc5e6fedcfa7efc
dcota <duartenunocota@gmail.com>
2021-06-22 12:44:28 +0000

    22jun2021

---
+++ TesteTIS_T1/src/controller/MainController.java
@@ -0,0 +1,99 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainController {
+
+    @FXML
+    private Button btnValidar;
+
+    @FXML
+    private Button btnSair;
+
+    @FXML
+    private TextField tfNum;
+
+    private String resposta;
+
+    @FXML
+    void validar(ActionEvent event) {
+        try {
+            //começar por verificar se a caixa de texto está vazia
+            if (this.tfNum.getText().isEmpty()) {
+                alertaAtencao("Tem de indicar um número!");
+            } else {
+                try {
+                    //tenta converter o valor introduzido para inteiro
+                    //se contém caracteres inválidos lança exceção apanhada na linha 77
+                    long num = Long.parseLong(this.tfNum.getText());
+                    //se o valor da caixa tiver mais do que 6 caracteres informa
+                    if (this.tfNum.getText().length() > 6) {
+                        alertaAtencao("O número tem mais do que 6 digitos");
+                    }
+                    //se não verifica se é primo e preenche a variável resposta
+                    else {
+                        boolean primo = true;
+                        for (int i = 2; i < num; i++) {
+                            if (num % i == 0) {
+                                primo = false;
+                                break;
+                            }
+                        }
+                        if (primo)
+                            resposta = "O número " + num + " é primo.";
+                        else
+                            resposta = "O número " + num + " não é primo.";
+                        //prepara e envia a resposta para a segunda vista
+                        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/second.fxml"));
+                        Parent root = loader.load();
+                        SecondController controller = loader.getController();
+                        controller.getResposta(resposta);
+                        //lança a segunda vista depois de passar a resposta
+                        Scene scene = new Scene(root);
+                        Stage stage = new Stage();
+                        stage.initModality(Modality.WINDOW_MODAL);
+                        stage.setScene(scene);
+                        stage.showAndWait();
+                        //depois de voltar da segunda vista limpa o formulário
+                        this.tfNum.setText("");
+                    }
+                } catch (NumberFormatException e) {
+                    alertaAtencao("Valor com caracteres inválidos!");
+                }
+            }
+        } catch (IOException e) {
+                alertaAtencao("Ocorreu um erro, tente de novo.");
+        }
+    }
+
+    //método para lançar alertas com mensagem variável
+    @FXML
+    void alertaAtencao(String txt){
+        Alert alerta = new Alert(Alert.AlertType.ERROR);
+        alerta.setHeaderText(null);
+        alerta.setTitle("ATENÇÃO");
+        alerta.setContentText(txt);
+        alerta.showAndWait();
+        this.tfNum.setText("");
+    }
+
+    //método para fechar a vista
+    @FXML
+    void sair(ActionEvent event) {
+        Stage stage = (Stage) this.btnSair.getScene().getWindow();
+        stage.close();
+    }
+
+}
+
